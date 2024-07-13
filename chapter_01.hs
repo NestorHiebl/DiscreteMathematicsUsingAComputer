26f97056a72120d17720a24bda37c32a3086c476
Nestor Hiebl <git@nestorhiebl.com>
2024-07-13 19:58:44 +0200

    Add chapter 1 exercises 6 to 8

--- chapter_01.hs
+++ chapter_01.hs
@@ -20,3 +20,29 @@ trim_space :: [Char] -> [Char]
 trim_space (' ':xs) = trim_space xs
 trim_space s = s
 
+-- Exercise 6: Suppose a program has read in a list of numbers of type Int.
+-- Each number is intended to represent a boolean value, where 0 means False,
+-- 1 means True, and any other number constitutes invalid input. Write a
+-- function convert :: [Int] -> [Bool] that converts a list of numbers to
+-- corresponding booleans.
+
+convert :: [Int] -> [Bool]
+convert [] = []
+convert (1:xs) = True : convert xs
+convert (0:xs) = False : convert xs
+convert (_:xs) = convert xs
+
+-- Exercise 7: Write a function member0 :: String -> Bool that takes a list of
+-- Char values and returns True if at least one of the characters is 0 and
+-- False otherwise. Hint: use map and the function or, which takes a list of
+-- boolean values and returns True if at least one of them is True.
+
+member0 :: String -> Bool
+member0 s = let helper c = if c == '0' then True else False
+            in or (map helper s)
+
+-- Exercise 8: Expand the following application:
+-- foldr max 0 [1,5,3]
+
+expanded = (max 1 (max 5 (max 3 0)))
+
