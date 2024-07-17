b75514e6f8499fbbec62675bc7622e132682f6c7
Nestor Hiebl <git@nestorhiebl.com>
2024-07-17 19:45:53 +0200

    Add chapter 3 exercises 1 to 3

--- chapter_03.hs
+++ chapter_03.hs
@@ -16,3 +16,27 @@ add :: Peano -> Peano -> Peano
 add Zero b = b
 add (Succ a) b = Succ (add a b)
 
+-- Exercise 1: Write a recursive function copy that copies its list argument.
+
+copy :: [a] -> [a]
+copy [] = []
+copy (x:xs) = x : copy xs
+
+-- Exercise 2: Write a function inverse that takes a list of pairs and swaps
+-- the pair elements.
+
+inverse :: [(a,b)] -> [(b,a)]
+inverse [] = []
+inverse (x:xs) = (snd x, fst x) : inverse xs
+
+-- Exercise 3: Write a function which takes two sorted lists and returns a
+-- sorted list containing the elements of each.
+
+merge :: Ord a => [a] -> [a] -> [a]
+merge xs [] = xs
+merge [] ys = ys
+merge (x:xs) (y:ys) = if x < y then
+                          x : merge xs (y:ys)
+                      else
+                          y : merge (x:xs) ys
+
