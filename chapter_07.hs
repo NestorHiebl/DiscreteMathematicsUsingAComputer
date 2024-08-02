95571f872ac106e9560c28ed92501124cde466c1
Nestor Hiebl <git@nestorhiebl.com>
2024-08-02 09:39:17 +0200

    Break apart chapters 6 and 7

--- chapter_07.hs
+++ chapter_07.hs
@@ -27,35 +27,3 @@ ex8d :: Bool
 ex8d = forall universe inner
     where inner y = forall universe (== (y + 1))
 
--- Note the existence of example_theorem in Stdm.lhs:
---  example_theorem =
---     Theorem
---         []
---         (Imp Q (Imp (And P R) (And R Q)))
---
--- proof1 represents a proof of example_theorem:
-
-proof1 :: Proof
-proof1 =
-    ImpI
-        (ImpI
-            (AndI
-                ((AndER
-                    (Assume (And P R))
-                    R),
-                Assume Q)
-            (And R Q))
-        (Imp (And P R) (And R Q)))
-    (Imp Q (Imp (And P R) (And R Q)))
-
-proof1_valid :: IO ()
-proof1_valid = check_proof example_theorem proof1
-
--- Exercise 22: Suppose we simply replace R ∧ Q below the {∧I} line with
--- Q ∧ R. This fixes the Invalid And-Introduction error, but it introduces
--- another error into the proof.
---  a) Edit proof2 to reflect this change; call the result proof3.
---  b) Decide exactly what is wrong with proof3.
---  c) Run the proof checker on proof3, and see whether it reports the same
---  error that you predicted.
-
